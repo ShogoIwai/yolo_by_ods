e0e36eea5f1e40b485a7f96103a41ba66757bc4d
iwai.shogo <iwai.shogo@megachips.co.jp>
2022-02-07 22:08:22 +0900

    updated pymongo revision

--- yolo_by_ma/run.sh
+++ yolo_by_ma/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-#python ./convert_maimages.py --dwn
-#pushd images
-#./down.sh
-#popd
+python ./convert_maimages.py --dwn
+pushd images
+bash ./down.sh
+popd
 python ./convert_maimages.py --conv
