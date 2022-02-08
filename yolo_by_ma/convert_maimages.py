6b2cc82230fb91a5aef612abedde84b917fe71b2
Shogo Iwai <iwai.shogo@megachips.co.jp>
2022-02-08 09:06:34 +0900

    mdified conv ma for gen down.sh

--- yolo_by_ma/convert_maimages.py
+++ yolo_by_ma/convert_maimages.py
@@ -20,15 +20,12 @@ imgsubdir = 'images'
 def parseOptions():
     argparser = ArgumentParser()
     argparser.add_argument('--dwn',  help=':generate download script', action='store_true') # use action='store_true' as flag
-    argparser.add_argument('--site', help=':specify site index') # use action='store_true' as flag
     argparser.add_argument('--snm',  help=':specify number of start page') # use action='store_true' as flag
     argparser.add_argument('--psz',  help=':specify number of page size') # use action='store_true' as flag
     argparser.add_argument('--lim',  help=':specify number of image on each name') # use action='store_true' as flag
     argparser.add_argument('--conv', help=':convert image files', action='store_true') # use action='store_true' as flag
     args = argparser.parse_args()
     if args.dwn: opts.update({'dwn':args.dwn})
-    if args.site: opts.update({'site':args.site})
-    else: opts.update({'site':0}) 
     if args.snm:  opts.update({'snm':args.snm})
     else: opts.update({'snm':1})   
     if args.psz:  opts.update({'psz':args.psz})
@@ -39,63 +36,64 @@ def parseOptions():
 
 if __name__ == '__main__':
     parseOptions()
+    start_num = int(opts['snm'])
+    page_size = int(opts['psz'])
+    lim = int(opts['lim'])
+
     if ('dwn' in opts.keys()):
         if os.path.isfile('site.txt'):
-            urlSiteAry = convert_darknettxt_dataset.csvread('site.txt')
-            site_idx = int(opts['site'])
-            urlSite = urlSiteAry[site_idx][0]
-
-            start_num = int(opts['snm'])
-            page_size = int(opts['psz'])
-
             names = []
-            try:
-                for i in range(page_size):
-                    idx = str(start_num + i)
-                    urlName = f"{urlSite}/{idx}/"
-                    print(f"loading : {urlName} ...")
-                    url = requests.get(urlName)
-                    soup = BeautifulSoup(url.content, 'html.parser')
 
-                    # for latest
-                    if (site_idx == 0):
-                        uls = soup.find_all('ul', class_='gallery-a e')
-                        if (not len(uls)): raise Exception
-                        for ul in uls:
-                            lis = ul.find_all('li')
-                            if (not len(lis)): raise Exception
-                            for li in lis:
-                                tas = li.find_all('a')
-                                if (not len(tas)): raise Exception
-                                name = tas[1].text
-                                # print(name)
-                                names.append(name)
-                    # top-rated
-                    if (site_idx == 1):
-                        uls = soup.find_all('ul', class_='gallery-a a d')
-                        if (not len(uls)): raise Exception
-                        for ul in uls:
-                            lis = ul.find_all('li')
-                            if (not len(lis)): raise Exception
-                            for li in lis:
-                                spans = li.find_all('span')
-                                if (not len(spans)): raise Exception
-                                name = spans[0].text
-                                # print(name)
-                                names.append(name)
-            except Exception:
-                pass
+            urlSiteAry = convert_darknettxt_dataset.csvread('site.txt')
+            for site_idx in range(2):
+                urlSite = urlSiteAry[site_idx][0]
+                try:
+                    for i in range(page_size):
+                        idx = str(start_num + i)
+                        urlName = f"{urlSite}/{idx}/"
+                        print(f"loading : {urlName} ...")
+                        url = requests.get(urlName)
+                        soup = BeautifulSoup(url.content, 'html.parser')
+
+                        # for latest
+                        if (site_idx == 0):
+                            uls = soup.find_all('ul', class_='gallery-a e')
+                            if (not len(uls)): raise Exception
+                            for ul in uls:
+                                lis = ul.find_all('li')
+                                if (not len(lis)): raise Exception
+                                for li in lis:
+                                    tas = li.find_all('a')
+                                    if (not len(tas)): raise Exception
+                                    name = tas[1].text
+                                    # print(name)
+                                    names.append(name)
+                        # top-rated
+                        if (site_idx == 1):
+                            uls = soup.find_all('ul', class_='gallery-a a d')
+                            if (not len(uls)): raise Exception
+                            for ul in uls:
+                                lis = ul.find_all('li')
+                                if (not len(lis)): raise Exception
+                                for li in lis:
+                                    spans = li.find_all('span')
+                                    if (not len(spans)): raise Exception
+                                    name = spans[0].text
+                                    # print(name)
+                                    names.append(name)
+                except Exception:
+                    pass
         else:
             names = ['cat', 'dog']
 
-        KeywordAry = convert_darknettxt_dataset.csvread('kwd.txt')
         exe = 'python ~/rep/yolo_by_ods/common/gid/gid.py'
-        lim = int(opts['lim'])
+        KeywordAry = convert_darknettxt_dataset.csvread('kwd.txt')
+        names = list(dict.fromkeys(names))
 
         dst_dir = f'./{imgsubdir}'
         if not os.path.isdir(dst_dir):
             os.makedirs(dst_dir)
-        downcsh = f'{dst_dir}/down%s.sh' % site_idx
+        downcsh = f'{dst_dir}/down.sh'
         ofs = open(downcsh, mode='w')
         ofs.write(f"#!/usr/bin/env bash\n")
         for name in names:
