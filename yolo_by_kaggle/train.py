e6130fa21ad60bbf94cf397fad5d8a808f4f875f
iwai.shogo <iwai.shogo@megachips.co.jp>
2022-02-14 22:25:04 +0900

    added evaluate func

--- yolo_by_kaggle/train.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-sys.path.append(os.path.abspath('./pytorch_yolov3'))
-
-import train_custom
-
-if __name__ == "__main__":
-    train_custom.main()
--- yolo_by_ma/run.sh
+++ yolo_by_ma/run.sh
@@ -7,4 +7,5 @@ python ./prepare_maimages.py --conv
 python ../common/cdd/convert_darknettxt_dataset.py ./ ./custom_classes.txt
 git clone https://github.com/nekobean/pytorch_yolov3
 wget https://pjreddie.com/media/files/darknet53.conv.74
-python train.py --dataset_dir ./ --weights ./darknet53.conv.74 --config ./yolov3_custom.yaml
+python ./pytorch_yolov3/train_custom.py --dataset_dir ./ --weights ./darknet53.conv.74 --config ./yolov3_custom.yaml
+python ./pytorch_yolov3/evaluate_custom.py --dataset_dir ./ --weight ./train_output/yolov3_final.pth --config ./yolov3_custom.yaml
